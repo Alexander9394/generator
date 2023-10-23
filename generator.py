64da6d82db150cb06b596502b7e0e4e3a6890e7f
Alexander Vingovatov <vingovatov.2022@stud.nstu.ru>
2023-10-23 19:34:33 +0700

    generator

---
+++ generator.py
@@ -0,0 +1,10 @@
+
+a = [
+    {'id': 1, 'full_name': 'Алекберов Рамиль Русланович'},
+    {'id': 2, 'full_name': 'Бобровская Анастасия Дмитриевна'},
+    {'id': 3, 'full_name': 'Винговатов Александр Олегович'}
+    ]
+
+D = [a['full_name'] for a in a if len(a['full_name'].split())]
+
+print(D)
